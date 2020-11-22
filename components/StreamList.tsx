79a9bbd5728d39ca10f4030057b959ae48c3fb72
Volkan Akkaya <volkanakkaya@Volkan-MacBook-Pro.local>
2020-11-22 21:32:52 +0300

    cleanup

--- components/StreamList.tsx
+++ components/StreamList.tsx
@@ -1,88 +1,190 @@
-import { View, Text, Icon, List, ListItem, Button } from "native-base";
+import { observer } from "mobx-react-lite";
+import { View, Text, Icon, Button } from "native-base";
 import React from "react";
-import { Inset, Queue } from "react-native-spacing-system";
-import { SwipeListView } from "react-native-swipe-list-view";
-import { StreamStoreContext } from "../stores/StreamStore";
+import AlertAsync from "react-native-alert-async";
+import { Stack } from "react-native-spacing-system";
 
-export const StreamList = () => {
-  const streamStore = React.useContext(StreamStoreContext);
+import { ProcessStream } from "../backend/processStream";
+import { ScrollView } from "react-native-gesture-handler";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+import { Animated } from "react-native";
 
-  return (
-    <SwipeListView
-      closeOnRowOpen={false}
-      closeOnScroll={true}
-      closeOnRowBeginSwipe={true}
-      closeOnRowPress={true}
-      rightOpenValue={-50}
-      rightActivationValue={-50}
-      disableRightSwipe
-      data={streamStore.streams}
-      renderItem={(stream) => (
-        <View
-          style={{
-            flex: 1,
-            flexDirection: "column",
-            backgroundColor: "#f4f4f4",
-            alignItems: "flex-start",
-            justifyContent: "center",
-          }}
-        >
-          <View
-            style={{ flexDirection: "row", alignItems: "center", padding: 5 }}
+interface StreamListInterface {
+  streams: ProcessStream[];
+  removeStream(arg: ProcessStream): void;
+  addStream(): void;
+}
+
+export const StreamList = observer(
+  ({ streams, removeStream, addStream }: StreamListInterface) => {
+    const [selectedStream, setSelectedStream] = React.useState<ProcessStream>();
+    const removeStreams = async () => {
+      const response = await AlertAsync(
+        "Tüm akımlar silinecek",
+        "Emin misiniz?",
+        [
+          {
+            text: "İptal",
+            onPress: () => "no",
+          },
+          {
+            text: "Tamam",
+            onPress: () => Promise.resolve("yes"),
+          },
+        ],
+        { cancelable: true, onDismiss: () => "no" }
+      );
+      if (response === "yes") streams.map((stream) => removeStream(stream));
+    };
+
+    const renderRightActions = (
+      progress: Animated.AnimatedInterpolation,
+      dragX: Animated.AnimatedInterpolation
+    ) => {
+      const trans = progress.interpolate({
+        inputRange: [0, 1],
+        outputRange: [64, 0],
+      });
+      return (
+        <View style={{ width: 64, flexDirection: "column-reverse" }}>
+          <Animated.View
+            style={{
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "flex-end",
+              transform: [{ translateX: trans }],
+            }}
           >
-            <Icon
-              type="Feather"
-              // name="check-circle"
-              name={stream.item.isCalculated ? "check-circle" : "alert-circle"}
-              style={{
-                fontSize: 18,
-                color: stream.item.isCalculated ? "green" : "red",
-                alignContent: "flex-start",
-              }}
-            />
-            <Queue size={10} />
-            <Text style={{ fontSize: 18, fontWeight: "500", color: "1a1c20" }}>
-              {stream.item.name}
-            </Text>
-          </View>
-          <View>
-            <List>
-              <ListItem>
-                <Text>{stream.item.result?.specificEnthalpy}</Text>
-              </ListItem>
-            </List>
-          </View>
+            <Button danger full>
+              <Text
+                style={{ color: "#f8f8f8", fontSize: 18, fontWeight: "300" }}
+                onPress={() => removeStream(selectedStream)}
+              >
+                Sil
+              </Text>
+            </Button>
+          </Animated.View>
         </View>
-      )}
-      renderHiddenItem={(stream) => (
+      );
+    };
+
+    return (
+      <View
+        padder
+        style={{
+          flex: 1,
+          flexDirection: "column",
+          padding: 0,
+          justifyContent: "flex-start",
+          alignItems: "stretch",
+        }}
+      >
         <View
           style={{
-            flex: 1,
-            paddingVertical: 0,
-            backgroundColor: "#f8f8f8",
             flexDirection: "row",
-            alignContent: "stretch",
-            justifyContent: "flex-end",
+            justifyContent: "space-between",
+            alignItems: "center",
+            borderBottomColor: "#1a1c20",
+            borderBottomWidth: 1,
+            paddingBottom: 10,
           }}
         >
-          <Button
-            full
-            danger
-            onPress={() => streamStore.removeStream(stream.item)}
+          <Text
+            style={{
+              fontSize: 20,
+              fontWeight: "300",
+              textAlign: "left",
+            }}
           >
-            <Text>Sil</Text>
+            Akımlar
+          </Text>
+          <Button
+            small
+            style={{ backgroundColor: "#d62632" }}
+            onPress={addStream}
+          >
+            <Icon fontSize={20} type="Feather" name="plus" />
           </Button>
         </View>
-      )}
-      ItemSeparatorComponent={() => (
+        <ScrollView>
+          {streams.map((stream, index) => (
+            <Swipeable
+              key={index}
+              rightThreshold={40}
+              friction={2}
+              onSwipeableRightOpen={() => setSelectedStream(stream)}
+              renderRightActions={renderRightActions}
+              children={
+                <Button
+                  full
+                  style={{
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                  }}
+                  onPress={() => alert(stream.result?.temperature.toFixed(1))}
+                >
+                  <View
+                    style={{
+                      flex: 1,
+                      height: 45,
+                      paddingHorizontal: 5,
+                      backgroundColor: "#f4f4f4",
+                      flexDirection: "row",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                      borderBottomWidth: 0.25,
+                      borderBottomColor: "#1a1c20",
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "#1a1c20",
+                        fontSize: 18,
+                        fontWeight: "200",
+                      }}
+                    >
+                      {stream.name}
+                    </Text>
+                    <Icon
+                      type="Feather"
+                      name={
+                        stream.isCalculated ? "check-circle" : "alert-circle"
+                      }
+                      style={{
+                        fontSize: 18,
+                        color: stream.isCalculated ? "green" : "red",
+                        alignContent: "flex-start",
+                      }}
+                    />
+                  </View>
+                </Button>
+              }
+            />
+          ))}
+        </ScrollView>
+        <Stack size={16} />
         <View
           style={{
-            backgroundColor: "gray",
-            height: 0.25,
-            paddingTop: 1,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            borderTopWidth: 1,
           }}
-        />
-      )}
-    />
-  );
-};
+        >
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: "100",
+            }}
+          >
+            Toplam {streams.length}
+          </Text>
+
+          <Button danger transparent onPress={removeStreams}>
+            <Text>Hepsini sil</Text>
+          </Button>
+        </View>
+      </View>
+    );
+  }
+);
--- screens/CalculateScreen.tsx
+++ screens/CalculateScreen.tsx
@@ -3,26 +3,17 @@ import { AdMobBanner } from "expo-ads-admob";
 import { observer } from "mobx-react-lite";
 import {
   Body,
-  Button,
-  Card,
   Container,
   Fab,
   Footer,
   Header,
   Icon,
-  Left,
-  List,
-  ListItem,
-  Right,
-  Text,
   Title,
   View,
 } from "native-base";
 import React from "react";
-import AlertAsync from "react-native-alert-async";
 import { StyleSheet } from "react-native";
 import { StreamStoreContext } from "../stores/StreamStore";
-import { SwipeListView } from "react-native-swipe-list-view";
 import { StreamList } from "../components/StreamList";
 
 interface CalculateScreenProps {
@@ -32,6 +23,9 @@ interface CalculateScreenProps {
 const CalculateScreen: React.FC<CalculateScreenProps> = observer(
   ({ navigation }) => {
     const streamStore = React.useContext(StreamStoreContext);
+    React.useEffect(() => {
+      console.log("n: ", streamStore.streams.length);
+    }, streamStore.streams);
     return (
       <Container style={{ flex: 1, flexDirection: "column" }}>
         <Header style={{ backgroundColor: "#F8F8F8" }}>
@@ -50,85 +44,12 @@ const CalculateScreen: React.FC<CalculateScreenProps> = observer(
               flex: 1,
               flexDirection: "column",
             }}
-          >
-            <Card style={{ height: "100%" }}></Card>
-          </View>
-          <View
-            padder
-            style={{
-              flex: 1,
-              flexDirection: "column",
-              padding: 0,
-              justifyContent: "flex-start",
-              alignItems: "stretch",
-            }}
-          >
-            <View style={{ flexDirection: "row", alignItems: "center" }}>
-              <Left>
-                <Text style={{ fontSize: 20, fontWeight: "300" }}>Akımlar</Text>
-              </Left>
-            </View>
-            <View
-              style={{
-                flexDirection: "row",
-                // borderBottomWidth: 1,
-                marginBottom: 5,
-              }}
-            >
-              <Left>
-                <Text
-                  style={{
-                    fontSize: 16,
-                    fontWeight: "100",
-                  }}
-                >
-                  Toplam {streamStore.streams.length}
-                </Text>
-              </Left>
-              <Text></Text>
-              <Right>
-                <Button
-                  danger
-                  transparent
-                  onPress={async () => {
-                    const response = await AlertAsync(
-                      "Tüm akımlar silinecek",
-                      "Emin misiniz?",
-                      [
-                        {
-                          text: "İptal",
-                          onPress: () => "no",
-                        },
-                        {
-                          text: "Tamam",
-                          onPress: () => Promise.resolve("yes"),
-                        },
-                      ],
-                      { cancelable: true, onDismiss: () => "no" }
-                    );
-                    if (response === "yes")
-                      streamStore.streams.map((stream) =>
-                        streamStore.removeStream(stream)
-                      );
-                    else {
-                      console.log(response);
-                    }
-                  }}
-                >
-                  <Text>Hepsini sil</Text>
-                </Button>
-              </Right>
-            </View>
-
-            <StreamList />
-          </View>
-          <Fab
-            style={{ backgroundColor: "#D76A03" }}
-            onPress={() => navigation.navigate("AddStream")}
-            position="bottomRight"
-          >
-            <Icon type="Feather" name="plus" style={{ color: "#fff" }} />
-          </Fab>
+          ></View>
+          <StreamList
+            streams={streamStore.streams}
+            removeStream={streamStore.removeStream}
+            addStream={() => navigation.navigate("AddStream")}
+          />
         </View>
         <Footer>
           <AdMobBanner
